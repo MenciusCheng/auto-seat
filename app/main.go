16be3941c0a777010996e75a39f7c440cfede6f6
marvel <295567172@qq.com>
2023-06-17 17:47:14 +0800

    优雅关闭，自动打开网页

--- app/main.go
+++ app/main.go
@@ -1,13 +1,56 @@
 package main
 
 import (
+	"context"
 	"github.com/MenciusCheng/auto-seat/server"
+	"github.com/MenciusCheng/auto-seat/util"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	r := server.InitRouter()
-	err := r.Run(":8080") // listen and serve on 0.0.0.0:8080
-	if err != nil {
-		panic(err)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
 	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	go func() {
+		time.Sleep(time.Second)
+		util.OpenBrowser("http://127.0.0.1:8080")
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 1 seconds.")
+	}
+	log.Println("Server exiting")
 }
